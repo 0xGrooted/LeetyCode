24ca8eefe459c3405dc8f7a44cd6525ef60a16f2
Mark M <markdrumbo@icloud.com>
2025-10-19 17:53:48 +0100

    Add Way_Too_Long_Words.cpp solution

---
+++ CodeForces/Way_Too_Long_Words.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word = "";
+    cin >> word;
+
+    if(word.length() > 10){
+        word = word[0] + to_string(word.length()-2) + word[word.length() - 1];
+    }
+    cout << word;
+    return 0;
+}
